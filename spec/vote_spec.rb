f1cf327b94970744f5ec2ec555ef71f2610f49bf
private <user@example.com>
2016-06-12 14:39:55 +0100

    Factor out a common layout

--- spec/vote_spec.rb
+++ spec/vote_spec.rb
@@ -8,15 +8,11 @@ describe 'Vote' do
     Vote.new
   end
 
-  it 'says whats for dinner' do
-    get '/'
-    expect(last_response).to be_ok
-    expect(last_response.body).to include('What\'s for dinner?')
-  end
-
   it 'uses template in the main title' do
     get '/'
+    expect(last_response).to be_ok
     expect(last_response.body).to include('<h1>Welcome to the Suffragist!</h1>')
+    expect(last_response.body).to include('What\'s for dinner?')
   end
 
   it 'adds the possibility to POST  results' do
