cbb8a272b608e92542d45053aa5277c92ceb4455
private <user@example.com>
2016-06-12 16:22:42 +0100

    Refactor tests

--- spec/vote_spec.rb
+++ spec/vote_spec.rb
@@ -1,7 +1,6 @@
 require 'vote'
 
 describe 'Vote' do
-
   include Rack::Test::Methods
 
   def app
@@ -11,26 +10,28 @@ describe 'Vote' do
   it 'uses template in the main title' do
     get '/'
     expect(last_response).to be_ok
-    expect(last_response.body).to include("<title>#{Messages::MAIN_TITLE}</title>")
-    expect(last_response.body).to include("<h1>#{Messages::MAIN_TITLE}</h1>")
+    expect_titles(Messages::MAIN_TITLE)
   end
 
   it 'adds the possibility to POST  results' do
     post '/cast', :vote => 'HAM'
     expect(last_response).to be_ok
-    expect(last_response.body).to include("<title>#{Messages::CAST_TITLE}</title>")
-    expect(last_response.body).to include("<h1>#{Messages::CAST_TITLE}</h1>")
+    expect_titles(Messages::CAST_TITLE)
     expect(last_response.body).to include('Hamburger')
   end
 
   it 'adds the results route and the results view' do
     get '/results'
     expect(last_response).to be_ok
-    expect(last_response.body).to include("<title>#{Messages::RESULTS_TITLE}</title>")
-    expect(last_response.body).to include("<h1>#{Messages::RESULTS_TITLE}</h1>")
+    expect_titles(Messages::RESULTS_TITLE)
     expect(last_response.body).to include('<th>Curry</th>')
     # expect(last_response.body).to include('<td>3</td>')
     # expect(last_response.body).to include('<td>###</td>')
+  end
+
+  def expect_titles(title)
+    expect(last_response.body).to include("<title>#{title}</title>")
+    expect(last_response.body).to include("<h1>#{title}</h1>")
   end
 
 end
